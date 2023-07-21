b0d3ee484eb8cfba65e3384cd1483125784effe9
seongjin0902 <ksj05099@naver.com>
2023-07-21 10:44:58 +0900

    0721

--- 03Filter/src/main/java/Servlet/LoginServlet.java
+++ 03Filter/src/main/java/Servlet/LoginServlet.java
@@ -1,13 +1,15 @@
 package Servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import Type.Role;
 
 @WebServlet("/login.do")
 public class LoginServlet extends HttpServlet{
@@ -31,9 +33,26 @@ public class LoginServlet extends HttpServlet{
 		System.out.println("userid : " + userid);
 		System.out.println("pwd : " + pwd);
 		
-		PrintWriter out = resp.getWriter();
-		out.println("USERID : " + userid);
-		out.print("PWD : " + pwd);
+//		PrintWriter out = resp.getWriter();
+//		out.println("USERID : " + userid);
+//		out.print("PWD : " + pwd);
+		
+//		ROLE 부여(in Session) - 일반계정 : user / 관리자 계정 : admin
+		HttpSession session = req.getSession();
+		
+		if(userid.equals("user"))
+		{
+			session.setAttribute("ROLE", Role.ROLE_USER);
+		}
+		else if(userid.equals("damin"))
+		{
+			session.setAttribute("ROLE", Role.ROLE_ADMIN);
+		}
+		
+		
+		
+		
+		
 	}
 
 }
